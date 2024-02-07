9e731b91cc11556b24fd999ad25805b12175237b
Nandang Eka Prasetya <72372613+naneps@users.noreply.github.com>
2024-02-07 11:20:33 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Vue 3 + Vite + Tailwind
+This is a template for creating a Vue 3 project with Vite and Tailwind CSS.
+
+## Versions
+| Vue | Vite | Tailwind |
+| --- | ---- | -------- |
+| 3.0.0 | 2.0.0-beta.65 | 2.0.0 |
+
+## Features
+- Vue 3
+- Vite
+- Tailwind CSS
+  
+## Requirements
+- Node.js
+- NPM
+- Git
+- A code editor
+- A browser
+- A terminal
+- A brain
+  
+  
+## Getting Started
+### Use Clone
+1. Clone this repository 
+2. Run `npm install`
+3. Run `npm run dev`
+4. Open `http://localhost:3000` in your browser
+5. Start coding!
+
+### Use Template
+1. Click the `Use this template` button
+2. Create a new repository
+3. Clone the new repository
+4. Run `npm install`
+5. Run `npm run dev`
+6. Open `http://localhost:3000` in your browser
+7. Start coding!
+8. When you are ready to deploy, run `npm run build` and the build files will be in the `dist` directory
+<!-- setup for name project int package.json -->
+## Setup
+1. Change the name of the project in `package.json`
+
+## Building for Production
+1. Run `npm run build`
+2. The build files will be in the `dist` directory
+3. You can serve the files with `npm run serve`
+
+<!-- make table for version -->
---
+++ src/App.vue
@@ -0,0 +1,71 @@
+  <template>
+  <main class="h-screen w-full bg-slate-800">
+    <div class="flex flex-col justify-center h-screen items-center">
+      <h1 class="text-4xl font-bold text-white text-animation">
+        Vite Vue Tailwindcss Starter
+      </h1>
+      <button
+        class="
+          bg-white
+          px-5
+          py-2
+          rounded-md
+          my-5
+          hover:bg-slate-400
+          transition-all
+          ease-in-out
+          duration-300
+          font-semibold
+        "
+        @click="cloneIt"
+      >
+        Starter VVT
+      </button>
+      {{ mouseX }}, {{ mouseY }}
+    </div>
+  </main>
+</template>
+<script>
+export default {
+  methods: {
+    cloneIt() {
+      window.open("https://github.com/naneps/vvt-starter/");
+    },
+  },
+};
+</script>
+<style>
+.text-animation {
+  /* make animation change random color */
+  animation: text-animation 1s infinite;
+  transition: all 0ms ease-in-out;
+}
+@keyframes text-animation {
+  0% {
+    color: #f56565;
+    transition: all 0.5s ease-in-out;
+    /* transform: rotate(2deg); */
+  }
+  25% {
+    color: #d1fe82;
+    transform: rotate(4deg);
+    /* transition: all 0.5s ease-in-out; */
+  }
+  50% {
+    color: #48bb78;
+    /* transition: all 0.5s ease-in-out; */
+    transform: rotate(0deg);
+  }
+  75% {
+    color: #4299e1;
+    transform: rotate(-4deg);
+    /* transition: all 0.5s ease-in-out; */
+  }
+  100% {
+    /* transition: all 0.5s ease-in-out; */
+    color: #f56565;
+    transform: rotate(-2deg);
+  }
+}
+/* make tail animation */
+</style> 
